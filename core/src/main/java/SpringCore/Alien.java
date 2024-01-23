00b967cf0ba2e4d49656ccfa99c80ba755b84101
Raguvaran <raguvaran2804@hotmail.com>
2024-01-23 22:13:31 +0530

    BeanFactory, ClassPathXMl via ApplicationContext, XML config Files, SetterInjection

---
+++ core/src/main/java/SpringCore/Alien.java
@@ -0,0 +1,33 @@
+package SpringCore;
+
+public class Alien {
+   private int age;
+   public Laptop laptop;
+
+
+    Alien(){
+        System.out.println("Alien Bean Created");
+    }
+    public void code(){
+
+        System.out.println("I am coding");
+        laptop.compile();
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        System.out.println("age assigned via setter");
+    }
+}
